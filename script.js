6fc9b90e8a91fa4190de335b2ca536f2871a82b0
KizuKoFaith <134709913+KizuKoFaith@users.noreply.github.com>
2023-08-28 15:59:00 +0800

    update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ fetch('/maindata.json')
     	let display = document.getElementById("items");
     	// Loop items from Json
 		for(let i = 0; i < data.length; i++){
-			let encode = "<div class='item w3-col m4 l3'><img src='"+data[i].cover+"'/><p id='title'>"+data[i].title+"</p></div>";
+			let encode = "";
 			display.innerHTML = encode;
 		}
   })
